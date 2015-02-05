db6f69ffd4789fdcce3af74d7a82b6d46c5286d9
Charles O'Farrell <charleso@charleso.org>
2015-02-06 09:37:23 +1100

    Introduce fromMaybeM

---
+++ test/P/MaybeTest.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE TemplateHaskell #-}
+module P.MaybeTest where
+
+import           P.Maybe
+
+import           Test.QuickCheck
+
+
+prop_fromMaybeM_identity :: (Eq a, Show a) => Maybe a -> Property
+prop_fromMaybeM_identity a = fromMaybeM Nothing a === a
+
+prop_fromMaybeM_just :: (Eq a, Show a) => a -> Property
+prop_fromMaybeM_just a = fromMaybeM (Left ()) (Just a) === Right a
+
+prop_fromMaybeM_nothing :: (Eq a, Show a) => a -> Property
+prop_fromMaybeM_nothing a = fromMaybeM (Left a) (Nothing :: Maybe ()) === Left a
+
+return []
+tests :: IO Bool
+tests = $quickCheckAll
--- test/test.hs
+++ test/test.hs
@@ -3,6 +3,7 @@ import           Control.Monad
 import qualified P.ApplicativeTest
 import qualified P.BoolTest
 import qualified P.EitherTest
+import qualified P.MaybeTest
 import qualified P.MonoidTest
 
 import           System.Exit
@@ -15,5 +16,6 @@ main =
       P.ApplicativeTest.tests
     , P.BoolTest.tests
     , P.EitherTest.tests
+    , P.MaybeTest.tests
     , P.MonoidTest.tests
     ] >>= \rs -> when (not . all id $ rs) exitFailure
