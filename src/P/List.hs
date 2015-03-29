ac6ea716afcbb2c3b37c892faae37fedab324583
Nick Hibberd <nhibberd@gmail.com>
2015-03-30 10:23:59 +1100

    Adding Ord nub

---
+++ src/P/List.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module P.List (
+    ordNub
+  ) where
+
+import Data.Eq
+import Data.Ord
+import Data.Function ((.))
+import Data.Functor (fmap)
+import Data.List hiding (head, group)
+import Data.List.NonEmpty (head, group)
+
+-- |
+-- Like `nub` from Prelude, but adds an `Ord` constraint to boost efficiency a little bit,
+-- though it could be improved even more with a Set or hashmap (with a `Hashable` constraint instead)
+-- WARNING: This is not stable due to sort
+ordNub :: (Ord a, Eq a) => [a] -> [a]
+ordNub = fmap head . group . sort
