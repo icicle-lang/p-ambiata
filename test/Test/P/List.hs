f085fe872f7c27030aa1eca85f924b7e3b7f3a46
Charles O'Farrell <charleso@charleso.org>
2015-04-24 09:37:24 +1000

    Introduce P.List lastMaybe

--- test/Test/P/List.hs
+++ test/Test/P/List.hs
@@ -11,6 +11,14 @@ prop_nub :: (Ord a, Eq a, Show a) => [a] -> Property
 prop_nub a =
   (L.sort . ordNub) a === (L.sort . L.nub) a
 
+prop_lastMaybe :: (Eq a, Show a) => a -> [a] -> Property
+prop_lastMaybe a l =
+  lastMaybe (l ++ [a]) === Just a
+
+prop_lastMaybe_empty :: Property
+prop_lastMaybe_empty =
+  lastMaybe [] === (Nothing :: Maybe ())
+
 return []
 tests :: IO Bool
 tests = $quickCheckAll
