2bdd909cce410691839bca8831a6014fff558453
Dom De Re <domdere@domdere.com>
2015-06-11 14:07:36 +1000

    Exporting common EitherT functions and the EitherT type

--- src/P/EitherT.hs
+++ src/P/EitherT.hs
@@ -1,10 +1,17 @@
 module P.EitherT (
-    firstEitherT
+    EitherT(..)
+  , eitherT
+  , bimapEitherT
+  , left
+  , hoistEither
+  , mapEitherT
+  , swapEitherT
+  , firstEitherT
   , secondEitherT
   , eitherTFromMaybe
   ) where
 
-import           Control.Applicative
+import           Control.Monad.Trans.Class ( MonadTrans(..) )
 import           Control.Monad.Trans.Either
 import           Data.Functor ()
 
@@ -12,7 +19,7 @@ firstEitherT :: (Functor m) => (e -> f) -> EitherT e m a -> EitherT f m a
 firstEitherT f = bimapEitherT f id
 
 secondEitherT :: (Functor m) => (a -> b) -> EitherT e m a -> EitherT e m b
-secondEitherT f = bimapEitherT id f
+secondEitherT = bimapEitherT id
 
-eitherTFromMaybe :: Functor m => b -> m (Maybe a) -> EitherT b m a
-eitherTFromMaybe l r = EitherT $ maybe (Left l) pure <$> r
+eitherTFromMaybe :: (Monad m) => b -> m (Maybe a) -> EitherT b m a
+eitherTFromMaybe l r = lift r >>= maybe (left l) return
