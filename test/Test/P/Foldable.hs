503bd8a821a7df0943b229cc4522300a5e41d15a
Jacob Stanley <jacob@stanley.io>
2015-12-21 10:17:16 +1100

    Build without warnings on 7.10

--- test/Test/P/Foldable.hs
+++ test/Test/P/Foldable.hs
@@ -2,27 +2,37 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Test.P.Foldable where
 
-import           Control.Applicative ( pure )
-import           Control.Monad.State
+import           Control.Applicative (pure)
+import           Control.Monad.State (StateT(..), State, runState)
+
+import           Data.Bool (Bool)
+import           Data.Either (Either(..))
+import           Data.Function (($), const)
+import           Data.Int (Int)
+import qualified Data.List as List
+import           Data.Maybe (Maybe(..))
 
 import           P.Foldable
 
+import           Prelude (Eq(..), Num(..))
+
+import           System.IO (IO)
+
 import           Test.QuickCheck
 
-import Prelude hiding ( head )
 
 prop_findMapM_first :: Int -> [Int] -> Property
 prop_findMapM_first x xs =
   runState (findMapM found (x : xs)) 0 === (Just $ x * 2, 1)
 
 prop_findMapM_last :: Int -> [Int] -> Property
-prop_findMapM_last x xs = notElem x xs ==>
+prop_findMapM_last x xs = List.notElem x xs ==>
   let f z = if z == x then found z else notfound
-  in runState (findMapM f (xs ++ [x])) 0 === (Just $ x * 2, length xs + 1)
+  in runState (findMapM f (xs List.++ [x])) 0 === (Just $ x * 2, List.length xs + 1)
 
 prop_findMapM_effects :: [Int] -> Property
 prop_findMapM_effects xs =
-  runState (findMapM (const notfound) xs) 0 === (Nothing, length xs)
+  runState (findMapM (const notfound) xs) 0 === (Nothing, List.length xs)
 
 
 prop_head_either_left :: Int -> Property
@@ -38,12 +48,12 @@ prop_head_list_empty :: Property
 prop_head_list_empty = head ([] :: [Int]) === Nothing
 
 found :: Int -> State Int (Maybe Int)
-found z = StateT $ \n -> return (Just $ z * 2, n + 1)
+found z = StateT $ \n -> pure (Just $ z * 2, n + 1)
 
 notfound :: State Int (Maybe Int)
-notfound = StateT $ \n -> return (Nothing, n + 1)
+notfound = StateT $ \n -> pure (Nothing, n + 1)
 
 
-return []
+pure []
 tests :: IO Bool
 tests = $quickCheckAll
