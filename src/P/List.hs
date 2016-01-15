eb4faf77cee39e3d3d9696aa6b72376774795296
Jacob Stanley <jacob@stanley.io>
2016-01-15 14:41:29 +1100

    Updates for GHC 8.0

--- src/P/List.hs
+++ src/P/List.hs
@@ -5,7 +5,6 @@ module P.List (
   , lastMaybe
   ) where
 
-import Data.Eq
 import Data.Ord
 import Data.Function ((.))
 import Data.Functor (fmap)
@@ -19,7 +18,7 @@ import Data.Int
 -- Like `nub` from Prelude, but adds an `Ord` constraint to boost efficiency a little bit,
 -- though it could be improved even more with a Set or hashmap (with a `Hashable` constraint instead)
 -- WARNING: This is not stable due to sort
-ordNub :: (Ord a, Eq a) => [a] -> [a]
+ordNub :: Ord a => [a] -> [a]
 ordNub = fmap head . group . sort
 
 lastMaybe :: [a] -> Maybe a
