b9186638f50fc06d90860df38e419d35df3254aa
Sharif Olorin <sio@tesser.org>
2016-05-02 01:13:45 +0000

    Add conversion functions for Maybe <-> Maybe'

--- src/P/Maybe.hs
+++ src/P/Maybe.hs
@@ -1,9 +1,23 @@
 module P.Maybe (
     fromMaybeM
+  , lazyMaybe'
+  , strictMaybe
   ) where
 
 import           Control.Applicative
+
+import           P.Maybe.Strict
+
 import           Prelude
 
 fromMaybeM :: Applicative f => f a -> Maybe a -> f a
 fromMaybeM = flip maybe pure
+
+
+strictMaybe :: Maybe a -> Maybe' a
+strictMaybe Nothing = Nothing'
+strictMaybe (Just x) = Just' x
+
+lazyMaybe' :: Maybe' a -> Maybe a
+lazyMaybe' Nothing' = Nothing
+lazyMaybe' (Just' x) = Just x
