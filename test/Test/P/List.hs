30f3db8338fd195fffdd548d4efef7c26eb3cb59
Navin Keswani <navin.keswani@ambiata.com>
2016-10-07 16:20:07 +1100

    Add safeIndex

--- test/Test/P/List.hs
+++ test/Test/P/List.hs
@@ -24,6 +24,16 @@ prop_count :: [Int] -> Fun Int Bool -> Property
 prop_count list (Fun _ predicate) =
   count predicate list === length (filter predicate list)
 
+prop_safe_index_is_safe :: Int -> Property
+prop_safe_index_is_safe idx =
+  let xs = [] :: [Int] in
+  xs `safeIndex` idx === Nothing
+
+prop_safe_index_correct :: Positive (Small Int) -> Property
+prop_safe_index_correct (Positive (Small idx)) =
+  let xs = [0..] :: [Int] in
+  xs `safeIndex` idx === Just (xs L.!! idx)
+
 return []
 tests :: IO Bool
 tests = $quickCheckAll
