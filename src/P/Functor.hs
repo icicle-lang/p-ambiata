407bc3eb3665e0f2b6bc5dfbfcc67e167148bc6d
Erik de Castro Lopo <erik.decastrolopo@ambiata.com>
2017-01-24 13:09:27 +1100

    Add double fmap operator

--- src/P/Functor.hs
+++ src/P/Functor.hs
@@ -2,11 +2,17 @@
 module P.Functor (
     module X
   , with
+  , (<$$>)
   ) where
 
 import           Data.Functor as X (Functor(..), ($>), (<$>), void)
+import           Data.Function((.))
 
 with :: Functor f => f a -> (a -> b) -> f b
 with xs f =
   fmap f xs
 {-# INLINE with #-}
+
+(<$$>) :: (Functor g, Functor f) => (a -> b) -> f (g a) -> f (g b)
+(<$$>) = fmap . fmap
+{-# INLINE (<$$>) #-}
